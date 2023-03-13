cb3fe27fc9b7f6306a81531334114f2309743f0b
Palesa Setai <PSetai@mpact.co.za>
2023-03-13 22:44:23 +0200

    Question_1

--- Inoxico.TechnicalQuestions.Answers/QuestionOne.cs
+++ Inoxico.TechnicalQuestions.Answers/QuestionOne.cs
@@ -3,13 +3,28 @@ namespace Inoxico.TechnicalQuestions.Answers
 {
     public class QuestionOne
     {
+        
         public static int GetLongestSentance(string s)
         {
-            throw new NotImplementedException();
+            int WordsCount = 0;
+            string sentencewithwords = string.Empty;
+            string[] arrAllSentences = s.Split(new char[] { ' ', ',', '.', '?', '!' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string strSentence in arrAllSentences)
+            {
+                string[] arrWords = strSentence.Split(new char[] { ' ', ',', '.', '?', '!' }, StringSplitOptions.RemoveEmptyEntries);
+                if (WordsCount < arrWords.Length)
+                {
+                    WordsCount = arrWords.Length;
+                    sentencewithwords = strSentence + ".";
+                }
+            }
+
+            return WordsCount;
         }
+
     }
 }
-
 
 
 
